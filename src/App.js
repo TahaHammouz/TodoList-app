479d1fe6f8518af715c77524876809c688304999
TahaHammouz <t.r.hamooz@gmail.com>
2023-01-04 16:52:03 +0200

    A toggle to show/hide todos.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { useState } from "react";
 function App() {
   const [todos, setTodos] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [hideDone, setHideDone] = useState(false);
   const handleAddTodo = (text) => {
     setTodos([...todos, { id: Date.now(), text, done: false }]);
   };
-  const filteredTodos = todos.filter((todo) => todo.text.includes(searchQuery));
+  const filteredTodos = todos
+    .filter((todo) => todo.text.includes(searchQuery))
+    .filter((todo) => !(hideDone && todo.done));
+
   const handleToggleTodo = (id) => {
     setTodos(
       todos.map((todo) => {
@@ -20,11 +24,23 @@ function App() {
   const handleSearchTodo = (query) => {
     setSearchQuery(query);
   };
+  const handleToggleHideDone = () => {
+    setHideDone(!hideDone);
+  };
 
   return (
     <div>
       <AddToDo onAddTodo={handleAddTodo} />
+
       <SearchTodo onSearchTodo={handleSearchTodo} />
+      <label>
+        <input
+          type="checkbox"
+          checked={hideDone}
+          onChange={handleToggleHideDone}
+        />
+        {hideDone ? "Show" : "Hide"} Done
+      </label>
       <ToDoList todos={filteredTodos} onToggleTodo={handleToggleTodo} />
     </div>
   );
