e0a595e068c50fb052391c7ed3627c8d57d224d3
TahaHammouz <t.r.hamooz@gmail.com>
2023-01-26 17:35:45 +0200

    test Header component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render} from "@testing-library/react";
+import { Header} from "./components/index";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("Header Component", () => {
+  it("Should render the Header component", () => {
+    const { getByTestId } = render(<Header />);
+    const headerElement = getByTestId("header");
+    expect(headerElement).toBeInTheDocument();
+  });
+  it("Should renders the svg element with the correct attributes", () => {
+    const { getByTestId } = render(<Header />);
+    const svgElement = getByTestId("header-svg");
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute("width", "126");
+    expect(svgElement).toHaveAttribute("height", "48");
+    expect(svgElement).toHaveAttribute("viewBox", "0 0 126 48");
+    expect(svgElement).toHaveAttribute("fill", "none");
+    expect(svgElement).toHaveAttribute("xmlns", "http://www.w3.org/2000/svg");
+  });
 });
